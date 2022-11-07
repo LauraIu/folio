f2fc50d37823272261f050ed379af712a0e90ba7
LauraIu <iuga.laura.katalin.com>
2022-11-07 15:33:49 +0100

    Updates

---
+++ precache-manifest.b8497294ab5acff264e3d569db77237c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "628e90d15aa6e1beea08",
+    "url": "/portfolio/static/css/main.29a3fe1a.chunk.css"
+  },
+  {
+    "revision": "628e90d15aa6e1beea08",
+    "url": "/portfolio/static/js/main.628e90d1.chunk.js"
+  },
+  {
+    "revision": "ee91f811fbb309dfe4b8",
+    "url": "/portfolio/static/js/1.ee91f811.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "7b50e166569cfa6fcde946ea940b6682",
+    "url": "/portfolio/static/media/webworks.7b50e166.png"
+  },
+  {
+    "revision": "4975dfefaeb92ae8d4f7473dfefa1bc4",
+    "url": "/portfolio/static/media/Iuga_Laura_portrait_04.4975dfef.png"
+  },
+  {
+    "revision": "0ce690d447a16414fd73e7bdedf5ecc5",
+    "url": "/portfolio/static/media/skills.0ce690d4.png"
+  },
+  {
+    "revision": "c62586661d575f77f74a8a9a886e7085",
+    "url": "/portfolio/static/media/video_07.c6258666.mp4"
+  },
+  {
+    "revision": "f78730b0de73520f31b5f0a49ab07624",
+    "url": "/portfolio/static/media/OtherWorks_IuLa.f78730b0.png"
+  },
+  {
+    "revision": "59f2d5bf5cf71283b18e51aadf995a1d",
+    "url": "/portfolio/static/media/portfolio_iula.59f2d5bf.svg"
+  },
+  {
+    "revision": "16b068687c8e5cf3c0b92e7bfb5974d9",
+    "url": "/portfolio/static/media/Book_a_Table.16b06868.svg"
+  },
+  {
+    "revision": "39cbaf702c102c29b55525411e50b031",
+    "url": "/portfolio/static/media/Social_Media_Dashboard_desktop1440w_dark_theme.39cbaf70.png"
+  },
+  {
+    "revision": "ecefe39502c4032cc9dbb87773d7cea9",
+    "url": "/portfolio/static/media/interactive_rating_component.ecefe395.png"
+  },
+  {
+    "revision": "6ae5a50acba126f7e3552ed063c9a800",
+    "url": "/portfolio/index.html"
+  }
+];
